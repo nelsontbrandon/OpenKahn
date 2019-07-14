cd901ae3331ba1668ec6fe857e05e51bfbd1a704
Brandon Taylor-Nelson <nelson.t.brandon+github@gmail.com>
2019-07-13 19:24:22 -0500

    fixed google strategy

--- auth/google.js
+++ auth/google.js
@@ -11,9 +11,9 @@ module.exports = function(basePath) {
 		clientID: process.env.GOOGLE_CLIENT_ID,
 		clientSecret: process.env.GOOGLE_CLIENT_SECRET,
 		callbackURL: basePath + 'auth/google/callback'
-	}), (accessToken, refreshToken, profile, done) => {
+	}, (accessToken, refreshToken, profile, done) => {
 
-	});
+	}));
 
 	router.get('/auth/google', passport.authenticate('google', {scope: ['https://www.googleapis.com/auth/plus.login']}));
 	router.get('/auth/google/callback', passport.authenticate('google', {
